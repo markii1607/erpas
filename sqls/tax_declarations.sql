e692dcbd931376ccd41a21613e66f7ea10685367
krisharie <bodosoktm@gmail.com>
2021-06-07 13:49:02 +0800

    updated tax_declarations table

--- sqls/tax_declarations.sql
+++ sqls/tax_declarations.sql
@@ -1,36 +1,30 @@
--- phpMyAdmin SQL Dump
--- version 4.9.1
--- https://www.phpmyadmin.net/
+-- MariaDB dump 10.17  Distrib 10.4.8-MariaDB, for Win64 (AMD64)
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 02, 2021 at 12:25 PM
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.11
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: erpas
+-- ------------------------------------------------------
+-- Server version	10.4.8-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `erpas`
---
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `tax_declarations`
 --
 
+DROP TABLE IF EXISTS `tax_declarations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tax_declarations` (
-  `id` int(5) UNSIGNED NOT NULL,
-  `revision_year_id` int(5) UNSIGNED NOT NULL,
+  `id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `revision_year_id` int(5) unsigned NOT NULL,
   `td_no` text NOT NULL,
   `pin` text NOT NULL,
   `owner` text NOT NULL,
@@ -41,7 +35,7 @@ CREATE TABLE `tax_declarations` (
   `beneficiary_address` text DEFAULT NULL,
   `beneficiary_tel_no` text DEFAULT NULL,
   `prop_location_street` text DEFAULT NULL,
-  `barangay_id` int(5) UNSIGNED NOT NULL,
+  `barangay_id` int(5) unsigned NOT NULL,
   `oct_tct_cloa_no` text DEFAULT NULL,
   `cct` text DEFAULT NULL,
   `survey_no` text DEFAULT NULL,
@@ -70,20 +64,23 @@ CREATE TABLE `tax_declarations` (
   `memoranda` text DEFAULT NULL,
   `status` int(5) DEFAULT 1 COMMENT '1-active | 2-retired | 3-canceled',
   `is_paid` tinyint(1) DEFAULT NULL,
-  `payment_details` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL
-) ;
+  `payment_details` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`payment_details`)),
+  `is_active` tinyint(1) NOT NULL DEFAULT 1,
+  `created_by` int(5) unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_by` int(5) unsigned NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tax_declarations`
---
-ALTER TABLE `tax_declarations`
-  MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-06-07 13:47:39
